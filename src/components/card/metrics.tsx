a9fe225e730910d31f9fe2f2de19a3fb2403d808
JocelynVelarde <a01285780@itesm.mx>
2023-11-25 13:01:17 -0600

    added cards

---
+++ src/components/card/metrics.tsx
@@ -0,0 +1,34 @@
+// components/Card.js
+
+import React from 'react';
+
+interface CardProps {
+  imageUrl: string;
+  title: string;
+  description: string;
+}
+
+const Card: React.FC<CardProps> = ({ imageUrl, title, description }) => {
+  return (
+    <a
+      href="#"
+      className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+    >
+      <img
+        className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+        src={imageUrl}
+        alt=""
+      />
+      <div className="flex flex-col justify-between p-4 leading-normal">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {title}
+        </h5>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {description}
+        </p>
+      </div>
+    </a>
+  );
+};
+
+export default Card;
--- src/pages/movilidad.tsx
+++ src/pages/movilidad.tsx
@@ -1,10 +1,11 @@
-import Nav from "~/components/Nav/Nav";
 import { useState } from "react";
 import Title from "~/components/general/title";
 import MapContainer from "~/components/movilidad/mapContainer";
 import { Trash2 } from "react-feather";
 import GooglePlacesAutocomplete from "~/components/movilidad/autocomplete";
 import { LoadScript } from "@react-google-maps/api";
+import Card from "~/components/card/metrics";
+import Nav from "~/components/Nav/Nav";
 
 interface location {
   lat: number;
@@ -28,40 +29,58 @@ const Movilidad = () => {
     <>
       <LoadScript googleMapsApiKey="AIzaSyApYvTkH-7FbW4paDE7mUXqNxT56srw6ec">
         <Nav />
-        <div className="z-20 h-screen w-full bg-slate-50 p-8 pt-16">
-          <Title title="Movilidad y rutas" />
+        <div className="z-20 h-screen w-full bg-slate-50 p-8 pt-16 flex">
+          <div style={{ flex: 1.5, borderRadius: "15px", overflow: "hidden" }}>
+            <MapContainer locations={locations} />
+          </div>
 
-          <div style={{ display: "flex", flexDirection: "row" }}>
-            {/* MapContainer takes the full width of the container */}
-            <div style={{ flex: 1.5, borderRadius: "0px", overflow: "hidden" }}>
-              {/* MapContainer takes the full width of the container */}
-              <MapContainer locations={locations} />
-            </div>
-
-            <div style={{ flex: 1, overflowY: "auto" }}>
-              {[...Array(autocompleteCount)].map((_, index: number) => (
-                <div
-                  key={index}
-                  className="relative mx-2 my-2 flex items-center rounded border p-4 shadow-md"
-                >
-                  <GooglePlacesAutocomplete setPlaces={setLocations} places={locations}/>
-                  <button
-                    onClick={() => handleRemoveAutocomplete(index)}
-                    className="ml-auto cursor-pointer text-red-500"
-                  >
-                    <Trash2 size={16} />
-                  </button>
-                </div>
-              ))}
-              <button
-                onClick={handleAddAutocomplete}
-                className="mx-2 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+          <div style={{ flex: 1, overflowY: "auto", marginLeft: "16px" }}>
+            {[...Array(autocompleteCount)].map((_, index: number) => (
+              <div
+                key={index}
+                className="relative mx-2 my-2 flex items-center rounded border p-4 shadow-md"
               >
-                Add Autocomplete
-              </button>
-            </div>
+                <GooglePlacesAutocomplete
+                  setPlaces={setLocations}
+                  places={locations}
+                />
+                <button
+                  onClick={() => handleRemoveAutocomplete(index)}
+                  className="ml-auto cursor-pointer text-red-500"
+                >
+                  <Trash2 size={16} />
+                </button>
+              </div>
+            ))}
+            <button
+              onClick={handleAddAutocomplete}
+              className="mx-2 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+            >
+              Add Autocomplete
+            </button>
           </div>
         </div>
+
+        <div className="flex justify-around p-8" style={{ marginTop: "-220" }}>
+  <Card
+    imageUrl="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.elmira.es%2Fwp-content%2Fuploads%2F2020%2F12%2Fgasolina-gasolinera-combustible.jpg&f=1&nofb=1&ipt=4e835daa87d4666f5d9bbf9c3762ebc32787779ee8d0ad0c0221a27b54a75fdc&ipo=images"
+    title="Costo total"
+    description="$525.25"
+    
+  />
+  <Card
+    imageUrl="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftrackermaster.com%2Fwp-content%2Fuploads%2F2019%2F09%2Fplanificador-de-rutas-de-reparto.jpg&f=1&nofb=1&ipt=9309fe7e67c66ae2377979e82489666c1e7436993ed20e0cc4234690b9fab2d3&ipo=images"
+    title="Maxima distancia entre rutas"
+    description="15 km"
+    
+  />
+  <Card
+    imageUrl="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.elmira.es%2Fwp-content%2Fuploads%2F2020%2F12%2Fgasolina-gasolinera-combustible.jpg&f=1&nofb=1&ipt=4e835daa87d4666f5d9bbf9c3762ebc32787779ee8d0ad0c0221a27b54a75fdc&ipo=images"
+    title="Cantidad de litros"
+    description="680 L"
+  />
+</div>
+
       </LoadScript>
     </>
   );
