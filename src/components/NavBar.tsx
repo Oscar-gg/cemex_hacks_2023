0e2f07ea5d2a02d807607e7483de15016885e713
mar-hz <103940408+mar-hz@users.noreply.github.com>
2023-11-25 13:15:19 -0600

    bg fix and routing

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,11 +8,11 @@ export default function NavBar() {
         <nav className="flex flex-row justify-between bg-sky-900 py-5">
             <img
                 src="/Cemex_logo.png"
-                className="ml-2 h-12 rounded-md bg-white p-2"
+                className="ml-4 h-12 rounded-md bg-white p-2"
             />
             <div className="flex flex-row items-center justify-evenly">
                 <p className="pr-16 text-right text-base font-semibold text-white hover:text-white/90" onClick={() => signIn()}>
-                    {sessionData ? `Welcome ${sessionData.user.name}!` : "Sign up"}
+                    {sessionData ? `Welcome ${sessionData.user.name}!` : "Sign in"}
                 </p>
                 {sessionData && <p className="pr-16 text-right text-base font-semibold text-white hover:text-white/90" onClick={() => signOut()}>Sign out</p>}
             </div>
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import NavBar from "~/components/NavBar";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { router } from "@trpc/server";
 
 export default function Dashboard() {
     const { data: sessionData, status } = useSession();
@@ -13,7 +14,14 @@ export default function Dashboard() {
     return (
         <>
         <NavBar />
-        <div className="container min-w-full min-h-screen flex flex-col bg-gradient-radial from-sky-100/90 to-white">
+        <div className="absolute inset-0 bg-cover bg-center z-[-1]">
+          <img
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.freepik.com%2Ffree-vector%2Fblue-pink-halftone-background_53876-99004.jpg&f=1&nofb=1&ipt=7f63a6a474411a04136290329f568a99c5ed3e2b0e6358792310beb6750e9009&ipo=images"  
+            alt="Background"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="container min-w-full min-h-screen flex flex-col">
             <div className="flex flex-row pt-14 px-20">
                 <div className="w-2 bg-stone-700 rounded-sm"></div>
                 <p className="text-lg font-semibold text-slate-700 px-6 text-left">
@@ -24,9 +32,9 @@ export default function Dashboard() {
                 </p>
             </div>
             <div className="flex flex-col md:flex-row items-center justify-center min-w-full gap-10 px-10 py-20">
-                <Card text="Monitoreo de Energía"/>
-                <Card text="Rutas Inteligentes"/>
-                <Card text="Identificación de Residuos"/>
+                <Card text="Monitoreo de Energía" page="/monitoreo"/>
+                <Card text="Rutas Inteligentes" page="/movilidad"/>
+                <Card text="Identificación de Residuos" page="/residuos"/>
             </div>
         </div>
         </>
@@ -34,13 +42,19 @@ export default function Dashboard() {
 }
 
 interface cardprops {
-    text: string,
+    text: string;
+    page: string;
 }
 
-function Card({ text }: cardprops) {
+function Card({ text, page }: cardprops) {
+    const router2 = useRouter();
+    const navigate = () => {
+        void router2.push(page);
+    };
+
     return (
         <>
-        <div className="bg-sky-600/70 px-4 py-4 h-40 w-80 flex flex-row items-center justify-center rounded-xl shadow-lg hover:shadow-xl hover:bg-sky-700/70">
+        <div className="bg-sky-600/70 px-4 py-4 h-40 w-80 flex flex-row items-center justify-center rounded-xl shadow-lg hover:shadow-xl hover:bg-sky-700/70" onClick={navigate}>
             <h2 className="text-white font-bold px-2 py-2 text-xl">{text}</h2>
         </div>
         </>
