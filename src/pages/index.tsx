0996a6a23d3fa8f2426f87ecd91c2262bf5fd47a
mar-hz <103940408+mar-hz@users.noreply.github.com>
2023-11-25 01:22:28 -0600

    actually good landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { signIn, useSession } from "next-auth/react";
 import Head from "next/head";
 
 import { api } from "~/utils/api";
+import Image from "../images/energy-11.png";
 
 export default function Home() {
   return (
@@ -11,13 +12,19 @@ export default function Home() {
         <meta name="description" content="no se que va aqui sos" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center px-50 bg-white">
-        <div className="container flex flex-col items-center justify-center px-4 py-16 h-full bg-sky-700/80 rounded-xl">
-          <h1 className="text-5xl font-extrabold tracking-tight text-sky-50 sm:text-9xl">
+      <nav className = "flex flex-row justify-between py-5 bg-sky-900">
+            <h1 className="text-white font-extrabold px-16 text-2xl">CEMEX</h1>
+            <div className="flex flex-row justify-evenly items-center">
+                <p className="text-white hover:text-white/90 pr-16 text-base font-semibold text-right">Sign up</p>
+            </div>
+      </nav>
+      <main className="flex min-h-screen flex-col items-center justify-center px-50 bg-gradient-radial from-sky-100/80 to-white">
+          <img className="w-16 h-16" src={Image.src}/>
+          <h1 className="text-5xl sm:text-8xl font-bold underline underline-offset-8 decoration-4 decoration-sky-300 text-sky-900 tracking-wide py-2 pt-12">
             CEMEX
           </h1>
-        </div>
-        <div className="container flex flex-col items-center justify-center px-4 py-16 ">
+          <p className="text-stone-900 text-xl py-4">Mantén un ambiente eficiente</p>
+        <div className="container flex flex-col items-center justify-center px-4 py-8 ">
           <AuthShowcase />
         </div>
       </main>
@@ -34,13 +41,14 @@ function AuthShowcase() {
   );
 
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
+    <div className="flex flex-row items-center justify-center gap-4">
       <button
-        className="rounded-full bg-sky-900/80 px-10 py-3 text-lg text-white font-bold no-underline transition hover:bg-sky-900/60"
+        className="rounded-full bg-sky-950 px-10 py-2 text-lg text-white font-semibold no-underline transition hover:bg-sky-800"
         onClick={() => void signIn()}
       >
-        Sign in
+        Log in
       </button>
+      
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,9 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
       fontFamily: {
         //sans: ["var(--font-sans)", ...fontFamily.sans],
       },
