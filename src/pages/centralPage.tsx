4cdabfc540c2e31cea4e260d8d86dec431a6923f
mar-hz <103940408+mar-hz@users.noreply.github.com>
2023-11-25 02:46:28 -0600

    pages

--- src/pages/centralPage.tsx
+++ src/pages/centralPage.tsx
@@ -1,3 +1,4 @@
+import { signOut } from "next-auth/react";
 
 export default function CenterPage() {
     return (
@@ -5,21 +6,25 @@ export default function CenterPage() {
         <nav className = "flex flex-row justify-between py-5 bg-sky-900">
             <h1 className="text-white font-extrabold px-16 text-2xl">CEMEX</h1>
             <div className="flex flex-row justify-evenly items-center">
-                <p className="text-white hover:text-white/90 px-10 text-base font-semibold text-right">Perfil</p>
-                <p className="text-white hover:text-white/90 pr-16 text-base font-semibold text-right">Sign Out</p>
+                <p className="text-white hover:text-white/90 px-10 text-base font-semibold text-right">
+                    Perfil
+                </p>
+                <p className="text-white hover:text-white/90 pr-16 text-base font-semibold text-right" onClick={() => void signOut()}>
+                    Sign Out
+                </p>
             </div>
         </nav>
-        <div className="container min-w-full min-h-screen flex flex-col bg-stone-100">
+        <div className="container min-w-full min-h-screen flex flex-col bg-gradient-radial from-sky-100/90 to-white">
             <div className="flex flex-row pt-14 px-20">
-                <div className="h-16 w-2 bg-stone-700 rounded-sm"></div>
-                    <p className="text-lg font-semibold text-slate-700 px-6 text-left">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Aliquam esse ducimus, voluptatum omnis illo expedita facilis adipisci
-                        dignissimos enim recusandae architecto veritatis commodi quibusdam,
-                        nihil delectus, quam reprehenderit. Ea, magni!
-                    </p>
-                </div>
-            <div className="flex flex-row items-center justify-center min-w-full gap-10 px-10 py-20">
+                <div className="w-2 bg-stone-700 rounded-sm"></div>
+                <p className="text-lg font-semibold text-slate-700 px-6 text-left">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Aliquam esse ducimus, voluptatum omnis illo expedita facilis adipisci
+                    dignissimos enim recusandae architecto veritatis commodi quibusdam,
+                    nihil delectus, quam reprehenderit. Ea, magni!
+                </p>
+            </div>
+            <div className="flex flex-col md:flex-row items-center justify-center min-w-full gap-10 px-10 py-20">
                 <Card text="Monitoreo de Energía"/>
                 <Card text="Rutas Inteligentes"/>
                 <Card text="Identificación de Residuos"/>
@@ -36,7 +41,7 @@ interface cardprops {
 function Card({ text }: cardprops) {
     return (
         <>
-        <div className="bg-sky-600/70 px-4 py-4 h-40 w-80 flex items-center justify-center rounded-xl shadow-lg hover:shadow-xl hover:bg-sky-700/70">
+        <div className="bg-sky-600/70 px-4 py-4 h-40 w-80 flex flex-row items-center justify-center rounded-xl shadow-lg hover:shadow-xl hover:bg-sky-700/70">
             <h2 className="text-white font-bold px-2 py-2 text-xl">{text}</h2>
         </div>
         </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 
 import { api } from "~/utils/api";
 import Image from "../images/energy-11.png";
+import Arrow from "../images/arrow-white.png";
 
 export default function Home() {
   return (
@@ -18,21 +19,21 @@ export default function Home() {
                 <p className="text-white hover:text-white/90 pr-16 text-base font-semibold text-right">Sign up</p>
             </div>
       </nav>
-      <main className="flex min-h-screen flex-col items-center justify-center px-50 bg-gradient-radial from-sky-100/80 to-white">
+      <main className="flex min-h-screen flex-col items-center justify-center px-50 bg-gradient-radial from-sky-100/90 to-white">
           <img className="w-16 h-16" src={Image.src}/>
           <h1 className="text-5xl sm:text-8xl font-bold underline underline-offset-8 decoration-4 decoration-sky-300 text-sky-900 tracking-wide py-2 pt-12">
             CEMEX
           </h1>
           <p className="text-stone-900 text-xl py-4">Mantén un ambiente eficiente</p>
         <div className="container flex flex-col items-center justify-center px-4 py-8 ">
-          <AuthShowcase />
+          <LoginButton />
         </div>
       </main>
     </>
   );
 }
 
-function AuthShowcase() {
+function LoginButton() {
   const { data: sessionData } = useSession();
 
   const { data: secretMessage } = api.post.getSecretMessage.useQuery(
@@ -41,14 +42,14 @@ function AuthShowcase() {
   );
 
   return (
-    <div className="flex flex-row items-center justify-center gap-4">
+    <div className="flex flex-row items-center justify-center">
       <button
-        className="rounded-full bg-sky-950 px-10 py-2 text-lg text-white font-semibold no-underline transition hover:bg-sky-800"
-        onClick={() => void signIn()}
+        className="flex flex-row items-center justify-center gap-4 rounded-full bg-sky-950 pr-8 pl-10 py-2 text-lg text-white font-semibold hover:bg-sky-800"
+        onClick={() => signIn()}
       >
         Log in
+        <img className="h-4 w-4" src={Arrow.src}></img>
       </button>
-      
     </div>
   );
 }
