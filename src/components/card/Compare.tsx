362d2dd24987b68952b3895e91d202a3e5d4bbc5
Oscar-gg <oscar.arreola.jr@gmail.com>
2023-11-26 09:54:00 -0600

    Changes

---
+++ src/components/card/Compare.tsx
@@ -0,0 +1,47 @@
+interface datos {
+  temperature: number;
+  lightLevel: number;
+  energyConsumption: number;
+}
+
+export const CompareCard = ({
+  officeName,
+  data,
+  time,
+}: {
+  officeName: string;
+  data: datos[];
+  time: string;
+}) => {
+  return (
+    <div className="max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow dark:border-gray-700 dark:bg-gray-800">
+      <h1 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+        {officeName}
+      </h1>
+
+      {data.length === 0 ? (
+        <p>No hay datos por desplegar</p>
+      ) : (
+        data.map((week, index) => (
+          <div className="my-2 p-2" id={week.lightLevel + " " + index}>
+            <p>Semana {index + 1}: </p>
+            <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+              Temperatura promedio: {week.temperature} °C
+            </p>
+
+            <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+              Nivel de luz: {week.lightLevel} (0-1024)
+            </p>
+            <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+              Consumo de Luz (kwh): {week.energyConsumption}
+            </p>
+          </div>
+        ))
+      )}
+
+      <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+        Ultima hora de actividad: {time}
+      </p>
+    </div>
+  );
+};
--- src/server/api/routers/office.ts
+++ src/server/api/routers/office.ts
@@ -2,57 +2,52 @@ import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { z } from "zod";
 
 export const officeRouter = createTRPCRouter({
-    addOffice: protectedProcedure
-        .input(z.object({ officeNum: z.number() }))
-        .mutation(async ({ ctx, input }) => {
-            return ctx.db.office.create({
-                data: {
-                    officeNum: input.officeNum
-                }
-            })
-        }),
+  addOffice: protectedProcedure
+    .input(z.object({ officeNum: z.number() }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.office.create({
+        data: {
+          officeNum: input.officeNum,
+        },
+      });
+    }),
 
-    getOffices: protectedProcedure
-        .query(async ({ ctx }) => {
-            const offices = await ctx.db.office.findMany({
-                orderBy: {
-                    officeNum: "asc"
-                }
-            })
+  getOffices: protectedProcedure.query(async ({ ctx }) => {
+    const offices = await ctx.db.office.findMany({
+      orderBy: {
+        officeNum: "asc",
+      },
+    });
 
-            const final = []
-            for (const office of offices) {
-                const temp = await ctx.db.temperature.findFirst({
+    const final = [];
+    for (const office of offices) {
+      const temp = await ctx.db.temperature.findFirst({
+        orderBy: {
+          timeStamp: "desc",
+        },
+        where: {
+          officeId: office.id,
+        },
+      });
 
-                    orderBy: {
-                        timeStamp: "desc"
-                    },
-                    where: {
-                        officeId: office.id
-                    },
+      const light = await ctx.db.light.findFirst({
+        orderBy: {
+          timeStamp: "desc",
+        },
+        where: {
+          officeId: office.id,
+        },
+      });
+      if (temp && light)
+        final.push({
+          office: office,
+          temp: temp,
+          light: light,
+          time: temp.timeStamp,
+        });
+      else final.push({ office: office, temp: 0, light: 0, time: "0:00" });
+    }
 
-
-                })
-
-                const light = await ctx.db.light.findFirst({
-
-                    orderBy: {
-                        timeStamp: "desc"
-                    },
-                    where: {
-                        officeId: office.id
-                    },
-
-
-                })
-                if (temp && light)
-                    final.push({ office: office, temp: temp.value, light: light.value, time: temp.timeStamp })
-
-                else
-                    final.push({ office: office, temp: 0, light: 0, time: "0:00" })
-
-            }
-
-            return final;
-        })
-})
+    return final;
+  }),
+});
