27db22e1b625c3352411896b58aeeada8a8350e5
Alejandra Coeto <a01285221@tec.mx>
2023-11-26 01:44:55 -0600

    comparativa

---
+++ src/pages/comparativa.tsx
@@ -0,0 +1,18 @@
+import Nav from "~/components/Nav/Nav"
+import Description from "~/components/general/description"
+import Title from "~/components/general/title"
+
+
+const Comparativa = () => {
+    return (
+        <>
+            <Nav />
+            <div className="container min-w-full min-h-screen flex flex-col bg-gradient-radial from-sky-100/90 to-white pt-24 px-10">
+                <Title title="Compartiva de oficinas" />
+                <Description description="Compara los gastos de las oficinas" />
+            </div>
+        </>
+    )
+}
+
+export default Comparativa
